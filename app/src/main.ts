2514490a1da9a75893871adc296cb9915747a1d5
Fanny Judicaël <fannyjudicael501@gmail.com>
2026-03-04 17:09:18 +0300

    feat(main.ts): set global validation pipe

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: (errors)=>{
+  
+        const formattedErrors = errors.map(error => ({
+          property: error.property,
+          contraint: Object.values(error.constraints ?? {})[0],
+        }));
+  
+        return new BadRequestException({
+          errors: formattedErrors,
+        });
+      },
+      stopAtFirstError: true,
+    })
+  );
+
+  app.enableCors({
+    origin: "*"
+  });
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
